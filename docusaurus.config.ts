d11d2b6984df5ede2574292396384fae146591a2
youssefea <youcefea99@gmail.com>
2024-03-05 15:16:30 +0000

    make docs on brand

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -63,13 +63,18 @@ const config: Config = {
   ],
   themeConfig: {
     // Replace with your project's social card
-    image: 'img/docusaurus-social-card.jpg',
+    //image: 'img/docusaurus-social-card.jpg',
+    colorMode: {
+      defaultMode: 'dark',
+      disableSwitch: true,
+      respectPrefersColorScheme: false,
+    },
     navbar: {
-      title: 'SuperBoring Docs',
+      title: 'Documentation',
       logo: {
         alt: 'SuperBoring',
         src: 'img/logo.svg',
-        href: '/docs/intro'
+        href: '/docs/'
       },
       items: [
         {
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,52 +1,6 @@
-import clsx from "clsx";
-import Link from "@docusaurus/Link";
-import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
-import Layout from "@theme/Layout";
-import HomepageFeatures from "@site/src/components/HomepageFeatures";
-import Heading from "@theme/Heading";
-import GeekWomanSVG from "@site/static/img/under-construction_geek_woman.svg";
-import styles from "./index.module.css";
+import React from 'react';
+import  { Redirect } from 'react-router-dom';
 
-function HomepageHeader() {
-  const { siteConfig } = useDocusaurusContext();
-  return (
-    <header className={clsx("hero hero--primary", styles.heroBanner)}>
-      <div className="container">
-        <Heading as="h1" className="hero__title">
-          {siteConfig.title}
-        </Heading>
-        <p className="hero__subtitle">{siteConfig.tagline}</p>
-        <div style={{ textAlign: "center" }}>
-          {" "}
-          {/* Center the SVG */}
-          <img
-            src="/img/under-construction-sign-icon.svg"
-            alt="Under Construction"
-            style={{ width: "200px" }}
-          />{" "}
-          {/* Adjust size as needed */}
-        </div>
-        <div className={styles.buttons}>
-          <Link
-            className="button button--secondary button--lg"
-            to="/docs/intro"
-          >
-            Start Here
-          </Link>
-        </div>
-      </div>
-    </header>
-  );
-}
-
-export default function Home(): JSX.Element {
-  const { siteConfig } = useDocusaurusContext();
-  return (
-    <Layout
-      title={`${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />"
-    >
-      <HomepageHeader />
-    </Layout>
-  );
+export default function Home() {
+  return <Redirect to='/docs/' />;
 }
