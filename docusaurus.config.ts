bd543a401eeb0aeba663640e62e9c0fda3de3404
youssefea <youcefea99@gmail.com>
2024-06-25 17:52:53 +0100

    change social card

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -63,7 +63,7 @@ const config: Config = {
   ],
   themeConfig: {
     // Replace with your project's social card
-    //image: 'img/docusaurus-social-card.jpg',
+    image: 'img/SocialCard.jpg',
     colorMode: {
       defaultMode: 'dark',
       disableSwitch: true,
